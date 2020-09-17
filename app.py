e7867607d3fafcebb597074f7c4247a44a825155
Aaron Sinnott <aaronsnig@gmail.com>
2020-09-17 19:55:59 -0300

    Add python-dotenv

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    """
-    """
+    """"""
     return render_template("index.html")
