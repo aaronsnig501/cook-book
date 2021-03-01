f88f304376fc7e25ff55bd4c623dc263ccc1846e
Aaron Sinnott <aaronsnig@gmail.com>
2021-03-01 10:10:58 -0300

    (feat): implement basic recipe page

--- app.py
+++ app.py
@@ -17,3 +17,90 @@ def index():
     - The `index.html` file
     """
     return render_template("index.html")
+
+
+@app.route("/recipes")
+def recipes():
+    """Recipe view
+
+    Handle the recipe route. This will get a list of the recipes to present to
+    users.
+
+    Returns:
+    - The `recipes.html` containing the recipes
+    """
+    recipes = [
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+        {
+            "name": "Chicken Curry",
+            "preparation_time": 10,
+            "ingredients": [{"name": "Chicken", "quantity": 400, "measurement": "g"}],
+            "image": "https://www.cookingclassy.com/wp-content/uploads/2018/08/chicken-curry-11-768x1152.jpg"
+        },
+    ]
+    return render_template("recipes.html", recipes=recipes)
