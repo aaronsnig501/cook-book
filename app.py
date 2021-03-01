98d31a2f133c7ba6fc0f1c8262ef03d73fcc5ff1
Aaron Sinnott <aaronsnig@gmail.com>
2021-03-01 15:28:32 -0300

    (feat): implement registration functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from os.path import join, dirname
-from flask import Flask, render_template
+import bcrypt
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 
@@ -8,6 +9,8 @@ dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get("SECRET_KEY")
 
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
@@ -23,6 +26,33 @@ def index():
     - The `index.html` file
     """
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register view
+
+    Handle the register view
+
+    Returns:
+    - The `register.html` file
+    """
+    if request.method == "POST":
+        username = request.form.get("username", None)
+        email = request.form.get("email", None)
+        password = request.form.get("password", None)
+
+        hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+        user = {"username": username, "email": email, "password": str(hashed)}
+
+        mongo.db.users.insert_one(user)
+
+        session["user"] = user["username"]
+        print(session)
+
+        return redirect(url_for("recipes"))
+    return render_template("accounts/register.html")
 
 
 @app.route("/recipes")
